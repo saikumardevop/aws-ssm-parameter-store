c3796c85cfb5cc5cc1ef5df7ae5a74b073ac9a39
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-03-04 17:12:03 +0530

    robosho.config

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_ssm_parameter" "parameters" {
   count = length(var.parameters)
-  name  = var.parameters [count.index].name
-  value = var.parameters [count.index].value
+  name  = var.parameters[count.index].name
+  value = var.parameters[count.index].value
   type  = "String"
   key_id = "021bd554-916f-454d-8d02-b25b0c244bdd"
 }
