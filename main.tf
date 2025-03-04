4f6225ec48f65bfb64310867bd5a6fba5639dd9e
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-03-04 11:53:34 +0530

    aws-parameter-store

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name  = "test.test1"
-  type  = "String"
   value = "Hello World"
+  type  = "SecureString"
+  key_id = "021bd554-916f-454d-8d02-b25b0c244bdd"
 }
