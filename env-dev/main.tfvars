2d340ca91acf7d31aa0dc10efa8ef39913a00253
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-03-04 12:19:09 +0530

    aws-parameter-store

---
+++ env-dev/main.tfvars
@@ -0,0 +1,4 @@
+parameters = [
+  { name = "dev.frontend.catalogue_url", value = "http://catalogue-dev.saikumar22.store:8080/" },
+  { name = "dev.frontend.cart_url", value = "http://cart-dev.saikumar22.store:8080/" }
+  ]
--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_ssm_parameter" "parameter" {
-  name  = "test.test1"
-  value = "Hello World"
-  type  = "SecureString"
+  count = length(var.parameters)
+  name  = var.parameters[count.index].name
+  value = var.parameters[count.index].value
+  type  = "String"
   key_id = "021bd554-916f-454d-8d02-b25b0c244bdd"
 }
