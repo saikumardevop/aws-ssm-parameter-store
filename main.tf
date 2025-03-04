06e9cc1b0c98fda3cfb0567c16961db3bfe74c28
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-03-04 15:57:04 +0530

    robosho.config

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_ssm_parameter" "parameter" {
+resource "aws_ssm_parameter" "parameters" {
   count = length(var.parameters)
   name  = var.parameters[count.index].name
   value = var.parameters[count.index].value
